060bcab911cbb9898172e0d87dc7e2e335052c0e
Chris Banning <chrisbanning510@gmail.com>
2026-05-06 03:37:36 -0400

    Add TechStack section with AWS, Google Cloud, Azure, Apple, Android, React, React Native, Node.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AppShowcase from './components/AppShowcase';
 import LogoCarousel from './components/LogoCarousel';
+import TechStack from './components/TechStack';
 // Remaining sections coming soon:
 // import Services from './components/Services';
 // import WhyUs from './components/WhyUs';
@@ -16,6 +17,7 @@ function App() {
       <Hero />
       <AppShowcase />
       <LogoCarousel />
+      <TechStack />
       {/* Additional sections will go here */}
     </>
   );
---
+++ src/components/TechStack.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './TechStack.module.css';
+
+const technologies = [
+  { name: 'AWS',          logo: '/logos/aws.svg' },
+  { name: 'Google Cloud', logo: '/logos/google-cloud.svg' },
+  { name: 'Azure',        logo: '/logos/azure.svg' },
+  { name: 'Apple',        logo: '/logos/apple.svg' },
+  { name: 'Android',      logo: '/logos/android.svg' },
+  { name: 'Microsoft',    logo: null },
+  { name: 'React',        logo: '/logos/react.svg' },
+  { name: 'React Native', logo: '/logos/react.svg' },
+  { name: 'Node.js',      logo: '/logos/nodejs.svg' },
+];
+
+const TechStack = () => (
+  <section className={styles.section}>
+    <p className={styles.label}>Technologies We Work With</p>
+    <div className={styles.grid}>
+      {technologies.map((tech) => (
+        <div key={tech.name} className={styles.item}>
+          <div className={styles.iconWrap}>
+            {tech.logo
+              ? <img src={tech.logo} alt={tech.name} className={styles.icon} />
+              : <span className={styles.iconText}>{tech.name}</span>
+            }
+          </div>
+          {tech.logo && <span className={styles.name}>{tech.name}</span>}
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default TechStack;
